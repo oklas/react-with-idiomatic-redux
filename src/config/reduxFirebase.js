4fb02d9edfafa8ea742996351ee30c188f02b1d3
Serguei Okladnikov <oklaspec@gmail.com>
2018-02-26 19:21:29 +0300

    fix firebase config export/import problem

--- src/config/reduxFirebase.js
+++ src/config/reduxFirebase.js
@@ -1,7 +1,9 @@
 // For more details, visit https://prescottprue.gitbooks.io/react-redux-firebase/content/config.html
-export const reduxFirebase = {
+const reduxFirebase = {
   userProfile: 'users', // root that user profiles are written to
   enableLogging: false, // enable/disable Firebase Database Logging
   // updateProfileOnLogin: false // enable/disable updating of profile on login
   // profileDecorator: (userData) => ({ email: userData.email }) // customize format of user profile
 }
+
+export default reduxFirebase

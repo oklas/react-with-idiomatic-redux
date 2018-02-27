4f4dd44de8f76bb756b224ae65d674f80cb45926
Serguei Okladnikov <oklaspec@gmail.com>
2018-02-27 12:19:56 +0300

    rewrite `todos` to load from firebase

--- src/components/todo/VisibleTodoList.js
+++ src/components/todo/VisibleTodoList.js
@@ -1,96 +1,73 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { compose, withHandlers } from 'recompose'
+import { populate, firebaseConnect } from 'react-redux-firebase'
 import { withRouter } from "react-router";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
-import * as actions from "../../actions";
-import { getVisibleTodos, getIsFetching, getErrorMessage } from "../../reducers";
+import { withNotifications } from '../../notifications'
+import { spinnerWhileLoading } from '../../utils'
 import TodoList from "./TodoList";
 import FetchError from "../FetchError";
 
-class VisibleTodoList extends Component {
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    componentDidUpdate( prevProps ) {
-        if ( this.props.filter !== prevProps.filter ) {
-            this.fetchData();
-        }
-    }
-
-    fetchData() {
-        const { filter, fetchTodos } = this.props;
-        fetchTodos( filter );
-    }
-
-    render() {
-        const { toggleTodo, filter, todos, isFetching, errorMessage } = this.props;
-
-        if ( isFetching && !todos.length ) {
-            return <p>Loading...</p>;
-        }
-
-        if ( errorMessage && !todos.length ) {
-            return (
-                <FetchError
-                  message={errorMessage}
-                  onRetry={() => this.fetchData()}
-                />
-            );
-        }
-
-        return (
-            <div>
-                { filter=='all' ? null :
-                  <BreadcrumbsItem to={filter}>
-                    {filter.charAt(0).toUpperCase() + filter.slice(1)}
-                  </BreadcrumbsItem>
-                }
-                <TodoList
-                  todos={todos}
-                  onTodoClick={toggleTodo}
-                />
-            </div>
-        );
-    }
-}
+const VisibleTodoList = ({toggleTodo, filter, todos}) => (
+   <div>
+     { filter=='all' ? null :
+       <BreadcrumbsItem to={filter}>
+         {filter.charAt(0).toUpperCase() + filter.slice(1)}
+       </BreadcrumbsItem>
+     }
+     <TodoList
+       todos={todos}
+       onTodoClick={toggleTodo}
+    />
+  </div>
+)
 VisibleTodoList.propTypes = {
     filter: PropTypes.string.isRequired,
     toggleTodo: PropTypes.func.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    errorMessage: PropTypes.string,
     todos: PropTypes.arrayOf( PropTypes.object ).isRequired,
-    fetchTodos: PropTypes.func.isRequired,
-};
-VisibleTodoList.defaultProps = {
-    errorMessage: "",
 };
 
-const mapStateToProps = ( state, { match: { params } } ) => {
-    let filter = "";
-    if ( !params.filter === undefined ) {
-        filter = "all";
-    } else if ( params.filter !== "all" && params.filter !== "active" && params.filter !== "completed" ) {
-        filter = "all";
-    } else {
-        filter = params.filter;
-    }
+const populates = [{ child: 'createdBy', root: 'users' }]
 
-    return {
-        todos: getVisibleTodos(
-                state,
-                filter
-            ),
-        isFetching: getIsFetching( state, filter ),
-        errorMessage: getErrorMessage( state, filter ),
-        filter,
-    };
-};
+const enhancer = compose(
+  withNotifications,
+  withRouter,
+  connect( ( {
+    firebase,
+    firebase: { auth, /*data: { projects } */ },
+  },
+  {
+    match: { params }
+  }) => ({
+    auth,
+    todos: Object.values(
+      populate(firebase, 'todos', populates) || {}
+    ),
+    filter:
+      params.filter !== "active" && params.filter !== "completed" ?
+        'all' : params.filter,
+    }),
+  ),
+  firebaseConnect(({filter}) => [{
+    path:'todos',
+    queryParams: [
+      'orderByChild=completed',
+      filter=='all' ? '' : 'equalTo=' + (filter=='completed')
+    ]
+  }]),
+  spinnerWhileLoading(['todos']),
+  withHandlers({
+    addTodo: ({firebase, showError}) => newTodo => (
+      firebase
+        .push('todos', newTodo)
+        .catch(err => {
+          showError('Error creating new todo: ' + error.message || error) // eslint-disable-line
+        })
+     )
+  })
+)
 
-const VisibleTodoListWithRouter = withRouter( connect(
-    mapStateToProps,
-    actions,
-)( VisibleTodoList ) );
+export default enhancer( VisibleTodoList )
 
-export default VisibleTodoListWithRouter;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,10 @@
 import { combineReducers } from "redux";
-import byId, * as fromById from "./byId";
-import createList, * as fromList from "./createList";
 import { reducer as form } from 'redux-form'
 import { firebaseReducer as firebase } from 'react-redux-firebase'
 
-const listByFilter = combineReducers( {
-    all: createList( "all" ),
-    active: createList( "active" ),
-    completed: createList( "completed" ),
-} );
-
 const appReducer = combineReducers( {
-    byId,
-    listByFilter,
     form,
     firebase
 } );
 
 export default appReducer;
-
-export const getVisibleTodos = (
-    state,
-    filter,
-) => {
-    const ids = fromList.getIds( state.listByFilter[ filter ] );
-    return ids.map( id => fromById.getTodo( state.byId, id ) );
-};
-
-export const getIsFetching = ( state, filter ) => fromList.getIsFetching( state.listByFilter[ filter ] );
-export const getErrorMessage = ( state, filter ) => fromList.getErrorMessage( state.listByFilter[ filter ] );
